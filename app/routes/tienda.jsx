1996af0fbe883e2d5b3e5b847747bf96fd2e8302
Nairet Vergara Forsyth <naiforsyth@gmail.com>
2023-10-12 11:17:50 -0300

    Anadir informacion dinamicamente

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,7 +1,52 @@
+import { useLoaderData } from 'react-router'
+import { getGuitarras } from '~/models/guitarras.server'
+import Guitarra from '~/components/guitarra'
+import styles from '~/styles/guitarras.css'
+
+export function meta() {
+  return [
+    { title: "GuitarLA - Tienda de Guitarras" },
+    { name: "description", content: "GuitarLA - Nuestra colección de guitarras" },  
+  ];
+}
+
+export function links(){
+  return[
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+
+export async function loader() {
+  const guitarras = await getGuitarras()
+  return guitarras.data
+}
 
 function Tienda() {
+
+  const guitarras = useLoaderData()
+
+
   return (
-    <div>tienda</div>
+    <main className='contenedor'>
+      <h2 className='heading'>Nuestra Colección</h2>
+
+      {guitarras?.length && (
+        <div className='guitarras-grid'>
+          {guitarras.map(guitarra => (
+            <Guitarra
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+
+        </div>
+      )}
+
+    </main>
   )
 }
 
